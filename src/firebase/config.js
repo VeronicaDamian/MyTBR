036e63beafda23f3522f212bf5279023c6774153
RobertOltean314 <olteanrobertadrian314@gmail.com>
2024-11-14 13:25:01 +0200

    Remove firebase/config.js from tracking and add to .gitignore

--- src/firebase/config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCLm4itgfS0SxA-BKoZer2Vd_B40XhlJ1Y",
-  authDomain: "my-tbr-339c7.firebaseapp.com",
-  projectId: "my-tbr-339c7",
-  storageBucket: "my-tbr-339c7.firebasestorage.app",
-  messagingSenderId: "486477094343",
-  appId: "1:486477094343:web:450dc9abba256f79470de2",
-};
